7e8208c8cdbad516018b0616f11061744366a629
RobertSO84 <robert.oropeza@gmail.com>
2023-05-19 18:12:11 -0600

    Finished task 3

--- shop-app-cdk/lib/shop-app-cdk-stack.ts
+++ shop-app-cdk/lib/shop-app-cdk-stack.ts
@@ -1,16 +1,16 @@
-import * as cdk from 'aws-cdk-lib';
+
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { ApiGatewayConstruct } from './stacks/gateway/ApiGatewayConstruct';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class ShopAppCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class ShopAppCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Persistance (DynamoDB)
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ShopAppCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Gateway (API Gateway + Lambda)
+    const gateway = new ApiGatewayConstruct(this, 'gateway')
   }
 }
