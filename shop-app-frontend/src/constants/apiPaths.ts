9f7552ec43e2ab9bb6f0c96757a8ac6cd1ac6beb
RobertSO84 <robert.oropeza@gmail.com>
2023-05-19 18:25:39 -0600

    apiPaths urls updated

--- shop-app-frontend/src/constants/apiPaths.ts
+++ shop-app-frontend/src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://2pg5p6zuk0.execute-api.us-east-2.amazonaws.com/dev/",
+  order: "https://2pg5p6zuk0.execute-api.us-east-2.amazonaws.com/dev/",
+  import: "https://2pg5p6zuk0.execute-api.us-east-2.amazonaws.com/dev/",
+  bff: "https://2pg5p6zuk0.execute-api.us-east-2.amazonaws.com/dev/",
+  cart: "https://2pg5p6zuk0.execute-api.us-east-2.amazonaws.com/dev/",
 };
 
 export default API_PATHS;
