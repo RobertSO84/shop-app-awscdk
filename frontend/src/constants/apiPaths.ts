56e4c3c69ef22fbe70db1016fee4ef011e43fd7c
RobertSO84 <robert.oropeza@gmail.com>
2023-06-15 20:10:10 -0600

    Products url api path added

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  products: "https://qwkewof0ih.execute-api.us-east-2.amazonaws.com/dev/",
+  products: "https://5b9ylu4q12.execute-api.us-east-2.amazonaws.com/prod/",
   order: "https://qwkewof0ih.execute-api.us-east-2.amazonaws.com/dev/",
   import: "https://qwkewof0ih.execute-api.us-east-2.amazonaws.com/dev/",
   cart: "https://qwkewof0ih.execute-api.us-east-2.amazonaws.com/dev/",
