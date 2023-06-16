4bc8bfe0413422bb5aa0323e482da78acbe7937e
RobertSO84 <robert.oropeza@gmail.com>
2023-06-15 19:48:04 -0600

    Frontend added

---
+++ frontend/src/constants/apiPaths.ts
@@ -0,0 +1,8 @@
+const API_PATHS = {
+  products: "https://qwkewof0ih.execute-api.us-east-2.amazonaws.com/dev/",
+  order: "https://qwkewof0ih.execute-api.us-east-2.amazonaws.com/dev/",
+  import: "https://qwkewof0ih.execute-api.us-east-2.amazonaws.com/dev/",
+  cart: "https://qwkewof0ih.execute-api.us-east-2.amazonaws.com/dev/",
+};
+
+export default API_PATHS;
